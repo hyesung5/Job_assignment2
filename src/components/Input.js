df56aa8bf2081fbecc9ebe95f86a389641ae7968
강혜성 <ganghyeseong@MacBook-Pro.local>
2020-09-16 18:55:04 +0900

    input 컴포넌트

---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Input extends React.Component {
+render(){
+    return(
+        <>
+        <input name={this.props.Name} placeholder={this.props.Placeholder} value={this.props.Value} onChange={this.props.OnChange} />
+
+        </>
+    )
+}
+}
+
+export default Input;
